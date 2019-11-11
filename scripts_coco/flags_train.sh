83bc16b895344d0d75e944ea86cef423e9049fdf
nganltp <nganltp98@gmail.com>
2019-11-11 17:37:22 +0700

    Updae

--- scripts_coco/flags_train.sh
+++ scripts_coco/flags_train.sh
@@ -2,4 +2,4 @@
 FLAG_PATH=/data/nganltp/data/flags
 RESULT_PATH=/data/nganltp/result/flags
 
-python train_setops_stripped_new.py --resume_path="" --inception_transform_input=False --init_inception=False --sets_basic_block_name=SetopResBasicBlock --sets_block_name=SetopResBlock_v1 --sets_network_name=SetOpsResModule --ops_latent_dim=8092 --ops_layer_num=2 --base_network_name=resnet50 --crop_size=224 --epochs=100 --train_base=True --train_classifier=True --coco_path=${FLAG_PATH} --results_path=${RESULT_PATH}
+python train_setops_stripped_new.py --resume_path="" --inception_transform_input=False --init_inception=False --sets_basic_block_name=SetopResBasicBlock --sets_block_name=SetopResBlock_v1 --sets_network_name=SetOpsResModule --ops_latent_dim=8092 --ops_layer_num=2 --base_network_name=resnet50 --crop_size=224 --epochs=50 --train_base=True --train_classifier=True --coco_path=${FLAG_PATH} --results_path=${RESULT_PATH}
